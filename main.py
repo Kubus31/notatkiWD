1040b2f5173a6ce582eba0a8014c66bd3f2ad183
Kubus31 <jakubbazelak@gmail.com>
2022-05-20 14:03:30 +0100

    dodanie notatek z wykresow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 ##############NUMPY
 
 # print(b.shape)  # Wyswietlanie ksztaltu macierzy
@@ -277,104 +277,151 @@ import matplotlib as plt
 
 ####filtrowanie, grupowanie, agregacja danych
 
-s = pd.Series([10, 12, 8, 14], index=['Ala', 'Marek', 'Wiesiek', 'Elonoora'])
+# s = pd.Series([10, 12, 8, 14], index=['Ala', 'Marek', 'Wiesiek', 'Elonoora'])
+# # print(s)
+#
+# data = {'Kraj' : ['Belgia','Indie', 'Brazylia', ],
+#        'Stolica' : ['Bruksela', 'New Delphi', "Brasilia"],
+#        'Populacja' : [11190846, 1303171035, 207847528]}
+#
+# df = pd.DataFrame(data)
+#
+# #wyswietlanie danych serii w zaleznosci od wartosci
+# print(s[s>9])
+# #lub
+# print(s.where(s>10))
+# #mozemy rownierz podmieniac wartosci
+# print(s.where(s>10, 'za duze'))
+# #mozemy podmienic wartosc w oryginale(domyslnie zwracana jest kopia)
+# seria = s.copy()
+# seria.where(seria > 10, 'za duze', inplace=True)
+# print(seria)
+#
+# #wyswietlanie wartosci mniejszych np. od 10
+# print(s[~(s>10)])
+# # mozemy rowniez loczyc warunki
+# print(s[(s<13)& (s>8)])
+#
+# ##warunki dla pobieranie data frame
+# print(df[df['Populacja']>1200000000])
+# # bardziej skomplikowane warunki
+# print(df[(df.Populacja)>1000000 & (df.index.isin([0,2]))])
+# # inny prsyklad z lista dopuszczalnych wartosci oraz isin
+# #zwacajaca wartosci boolowskie
+# print('#########')
+# szukaj = ['Belgia','Brasilia']
+# print(df.isin(szukaj))
+#
+# ## zmiana, usuwanie i dodawanie danych
+#
+# #w prypadku serii mozemy dodac/zmienic wartosc poprzez odwolanie sie
+# #do elementu serii przez klucz (index)
+#
+# s['Wiesiek'] = 15
+# print(s.Wiesiek)
+# s['Alan'] = 16
 # print(s)
-
-data = {'Kraj' : ['Belgia','Indie', 'Brazylia', ],
-       'Stolica' : ['Bruksela', 'New Delphi', "Brasilia"],
-       'Populacja' : [11190846, 1303171035, 207847528]}
-
-df = pd.DataFrame(data)
-
-#wyswietlanie danych serii w zaleznosci od wartosci
-print(s[s>9])
-#lub
-print(s.where(s>10))
-#mozemy rownierz podmieniac wartosci
-print(s.where(s>10, 'za duze'))
-#mozemy podmienic wartosc w oryginale(domyslnie zwracana jest kopia)
-seria = s.copy()
-seria.where(seria > 10, 'za duze', inplace=True)
-print(seria)
-
-#wyswietlanie wartosci mniejszych np. od 10
-print(s[~(s>10)])
-# mozemy rowniez loczyc warunki
-print(s[(s<13)& (s>8)])
-
-##warunki dla pobieranie data frame
-print(df[df['Populacja']>1200000000])
-# bardziej skomplikowane warunki
-print(df[(df.Populacja)>1000000 & (df.index.isin([0,2]))])
-# inny prsyklad z lista dopuszczalnych wartosci oraz isin
-#zwacajaca wartosci boolowskie
-print('#########')
-szukaj = ['Belgia','Brasilia']
-print(df.isin(szukaj))
-
-## zmiana, usuwanie i dodawanie danych
-
-#w prypadku serii mozemy dodac/zmienic wartosc poprzez odwolanie sie
-#do elementu serii przez klucz (index)
-
-s['Wiesiek'] = 15
-print(s.Wiesiek)
-s['Alan'] = 16
-print(s)
-
-#podobna operacja dla Data frame ma nieco inny efekt- wartosc
-#ustawiona dla wszystkichh kolumn
-df.loc[3] = 'dodane'
-print(df)
-#ale mcana dodac wiersz w postaci licty
-df.loc[4] = ['Polska','Warszawa', 38675467]
-print(df)
+#
+# #podobna operacja dla Data frame ma nieco inny efekt- wartosc
+# #ustawiona dla wszystkichh kolumn
+# df.loc[3] = 'dodane'
+# print(df)
+# #ale mcana dodac wiersz w postaci licty
+# df.loc[4] = ['Polska','Warszawa', 38675467]
+# print(df)
 
 #usuwanie danych moza wykonywac przez funkcje drop, alepamietajmy
 # ze operacja nie wykonuje sie in-place wiec
 #zwracana jest kopia DataFramr z usunietymi  wartosciami
 
-new_df = df.drop([3])
-print(new_df)
-#aby zmienic oryginal nalezy dodac inplace
-df.drop([3], inplace=True)
-print(df)
-#moza usuwac cale kolumny po nazwie indeksu ale wykonanie trj czynnosci
-#uniemozliwi dalsze wykonywanie kodu
-#df.drop('Kraj',axis=1, inplace=True)
+# new_df = df.drop([3])
+# print(new_df)
+# #aby zmienic oryginal nalezy dodac inplace
+# df.drop([3], inplace=True)
+# print(df)
+# #moza usuwac cale kolumny po nazwie indeksu ale wykonanie trj czynnosci
+# #uniemozliwi dalsze wykonywanie kodu
+# #df.drop('Kraj',axis=1, inplace=True)
+#
+# #do DataFrame mozemy dodawac rowniez kolumny zamiast wierszy
+# df['Kontynent']= ['Europa', 'Azja', 'Ameryka Poludniowa', 'Europa']
+# print(df)
+#
+# #Pandas ma rowniez wlasne funkcje sortowania danych
+# print(df.sort_values(by='Kraj'))
+#
+# #grupowania
+# grouped = df.groupby(['Kontynent'])
+# print(grouped.get_group('Europa'))
+#
+# # mozna tez jak w SQL czy Excelu uruchomic funkcje agregujace na danej kolumnie
+# print("###")
+# print(df.groupby(['Kontynent']).agg({'Populacja':['sum']}))
 
-#do DataFrame mozemy dodawac rowniez kolumny zamiast wierszy
-df['Kontynent']= ['Europa', 'Azja', 'Ameryka Poludniowa', 'Europa']
-print(df)
+###WYKRESY
+ts = pd.Series(np.random.randn(1000))
+#funkcja biblioteki pandas generujaca skumulowana
+#sume kolejnych elementow
+# ts = ts.cumsum()
+# print(ts)
+# ts.plot()
+# plt.show()
 
-#Pandas ma rowniez wlasne funkcje sortowania danych
-print(df.sort_values(by='Kraj'))
+# data = {'Kraj' : ['Belgia','Indie', 'Brazylia','Polska' ],
+#         'Stolica' : ['Bruksela', 'New Delphi', "Brasilia",'Warszawa'],
+#         'Kontynent':['Europa','Azja','Ameryka Poludnowa','Europa'],
+#         'Populacja' : [11190846, 1303171035, 207847528,38675467]}
+#
+# df = pd.DataFrame(data)
+# print(df)
+#
+# grupa = df.groupby(['Kontynent']).agg({'Populacja':['sum']})
+# print(grupa)
 
-#grupowania
-grouped = df.groupby(['Kontynent'])
-print(grouped.get_group('Europa'))
+#tworzenie wykresu
+# grupa.plot(kind='bar', xlabel='Kontynent',ylabel='Mld', rot=0,
+#            legend=True, title='Populacja z podzialem na kontynenty')
+# plt.show()
 
-# mozna tez jak w SQL czy Excelu uruchomic funkcje agregujace na danej kolumnie
-print("###")
-print(df.groupby(['Kontynent']).agg({'Populacja':['sum']}))
+##LUB
+
+# wykres = grupa.plot.bar()
+# wykres.set_ylabel("MLD")
+# wykres.set_xlabel('Kontynent')
+# wykres.tick_params(axis='x', labelrotation=0)
+# wykres.legend()
+# wykres.set_title('Populacja z podzialem na kontynenty')
+# plt.savefig('wykres.png')
+# plt.show()
+
+###wczytywanie danych z pliku i wyswietlanie zgrupowanych wartosci
+# df = pd.read_csv('dane.csv', header=0, sep=';', decimal='.')
+#
+# print(df)
+# grupa = df.groupby(['Imię i nazwisko']).agg({'Wartość zamówienia':['sum']})
+
+#wykres kolumnowy z wartosciami procentowymi sformatowanymmi z dokladnoscia
+# do 2 miejsc po przecinku
+#figsize ustawia wielkosc wykresu w calach,  domyslnie [6.4, 4.8]
+
+# grupa.plot(kind='pie', subplots=True, autopct='%.2f%%', fontsize=20, figsize=(6, 6),
+#            colors=['red', 'green'])
+#
+# plt.legend(loc="lower right")
+# plt.title('Suma zamowienia dla sprzedawcy')
+# plt.show()
 
 
+##wykres sredniej kroczącej
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# ts = ts.cumsum()
+# df = pd.DataFrame(ts, columns=['wartosci'])
+# #dodanie nowej kolumny i wykorzystanie funkcji rolling do stworzniea
+# #kolejnych wrtosci średniej kroczącej
+# df['Średnia krocząca'] = df.rolling(window=50).mean()
+# df.plot()
+# plt.legend()
+# plt.show()
 
 
 
