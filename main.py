82ffef69d9f8a195dfcc804a554f7ae2bfba73e3
Kubus31 <jakubbazelak@gmail.com>
2022-05-29 06:50:47 +0100

    aktualizacja notatek

--- main.py
+++ main.py
@@ -431,7 +431,134 @@ ts = pd.Series(np.random.randn(1000))
 # plt.legend()
 # plt.show()
 
+from PIL import Image
+# plt.plot([1,2,3,4],[5,6,7,8])
+# plt.ylabel('wartosci')
+# plt.show()
+#
+# plt.plot([1,2,3,4],[1,4,9,16], 'r:')
+# plt.plot([1,2,3,4],[1,4,9,16], 'bo')
+#
+# plt.axis([0,6,0,20])
+# plt.show()
+#
+# t = np.arange(0,5, 0.1)
+# plt.plot(t, t, 'r-', t, t**2, 'b:', t,t**3, 'g')
+# plt.legend(labels=['liniowa', 'kwadratowa', 'szescnienna'], loc= 'center left')
+# plt.show()
+#
+# x = np.linspace(0, 2, 100)
+#
+# plt.plot(x, x, label= 'liniowa')
+# plt.plot(x, x**2, label= 'kwadratowa')
+# plt.plot(x, x**3, label= 'szecienna')
+# plt.xlabel('etykieta osi x')
+# plt.ylabel('etykieta osi y')
+# plt.title('Wykres trzech linii')
+#
+# plt.savefig('plot.png')
+# plt.show()
+#
+# im1 = Image.open('plot.png')
+# im1 = im1.convert('RGB')
+# im1.save('plot1.jpg')
+
+# x = np.arange(1, 20)
+# y = 1/x
+# plt.plot(x, y, 'o-')
+# plt.show()
+
+# x = np.arange(1, 20)
+# y = np.sin(x)
+#
+# plt.plot(x, y, 'ro', label='F(x) = sinx')
+# plt.legend(labels=['F(x) = sinx'])
+# plt.grid()
+# plt.xlabel('wartosc x')
+# plt.ylabel('wartosc F(x)')
+# plt.show()
+
+# x1 = np.arange(0, 2, 0.02)
+# x2 = np.arange(0, 2, 0.02)
+#
+# y1 = np.sin(2 * np.pi * x1)
+# y2 = np.cos(2 * np.pi * x2)
+# #
+# plt.subplot(2, 1, 1)
+# plt.plot(x1, y1)
+# plt.ylabel('sin(x)')
+# plt.title('wykres sin(x)')
+#
+# plt.subplot(2, 1, 1)
+# plt.plot(x2, y2, 'r-')
+# plt.ylabel('cos(x)')
+# plt.title('wykres cos(x)')
+# plt.show()
 
 
+# fig, axs = plt.subplots(3, 2)
+#
+# axs[0, 0].plot(x1, y1, 'g-')
+# axs[0, 0].set_xlabel('x')
+# axs[0, 0].set_ylabel('y')
+# axs[0, 0].set_title('wyres sin(x)')
+#
+#
+#
+# axs[1, 1].plot(x2, y2, 'r-')
+# axs[1, 1].set_xlabel('x')
+# axs[1, 1].set_ylabel('cos(x)')
+# axs[1, 1].set_title('Wykres cos(x)')
+#
+# axs[2, 0].plot(x2, y2, 'r-')
+# axs[2, 0].set_xlabel('x')
+# axs[2, 0].set_ylabel('cos(x)')
+# axs[2, 0].set_title('Wykres cos(x)')
+#
+# fig.delaxes(axs[0, 1])
+# fig.delaxes(axs[1, 0])
+# fig.delaxes(axs[2, 1])
+# plt.show()
 
 
+###########WWYkresy
+
+
+# data = {'a':np.arange(50),
+#         'c':np.random.randint(0, 51 ,50),
+#         'd':np.random.rand(50)}
+#
+# data['b'] = data['a']+10 * np.random.random(50)
+# data['d'] = np.abs(data['d']) *100
+#
+# plt.scatter(data=data, x= 'a', y='b', c='c', s='d' )
+# plt.xlabel('wartosci z klucza a')
+# plt.ylabel('wartosci klocza b')
+# #plt.show()
+#
+# data = {'Kraj' : ['Belgia','Indie', 'Brazylia', 'Polska' ],
+#       'Stolica' : ['Bruksela', 'New Delphi', "Brasilia", 'Warszawa'],
+#       'Populacja' : [11190846, 1303171035, 207847528, 20784478],
+#         'Kontynent':['Europa', 'Azja', 'Ameryka Południowa', 'Europa']}
+#
+# df = pd.DataFrame(data)
+# print(df)
+# grupa = df.groupby('Kontynent')
+# etykiety = list(grupa.groups.keys())
+# wartosci = list(grupa.agg('Populacja').sum())
+# print(etykiety)
+# print(wartosci)
+# plt.bar(x=etykiety, height=wartosci, color=['red','green','blue'])
+# plt.xlabel('Kontynent')
+# plt.ylabel('Populacja na kontynentach')
+# #plt.show()
+#
+# x = np.random.randn(10000)
+# plt.hist(x, bins=50, facecolor='g', alpha=0.75, density=True)
+# plt.xlabel('Wartosci')
+# plt.ylabel('Prawdopodobienstwa')
+# plt.title('Histogram')
+# plt.show()
+#
+#
+#
